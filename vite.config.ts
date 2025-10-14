560dc01e7221ee1ad3167c1bb6970a65436f7ee8
zvlasic <zlayo1@gmail.com>
2025-10-14 14:33:36 +0200

    Setup gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/'
 }));
